70cdc1ccb0942014f3522a57771e3bdc3a2186c6
Adam Allport <adam@allport.me.uk>
2018-01-30 14:17:45 +0000

    Moving some comments 'round

--- Lesson-01/01-HelloWorld.py
+++ Lesson-01/01-HelloWorld.py
@@ -6,6 +6,6 @@ print("Hello World!")  # Output:>Hello World!
 # This can be useful when the text to be printed is known from the start, but this can get tedious
 
 # Lets create a variable
-spam = "eggs"  # monty python reference. Fun fact, the name python was inspired by monty python franchise
+spam = "eggs"  # monty python reference. Fun fact, the name Python was inspired by Monty Python franchise
 print(spam)  # Output:>eggs
 # What???, we said spam, but it printed "eggs", do not fear, variables are here
--- Lesson-01/02-SimpleUserInput.py
+++ Lesson-01/02-SimpleUserInput.py
@@ -8,14 +8,14 @@ target = 0  # we put this here to keep it in "scope"
 while True:
     try:
         target = int(input("Please enter a number: "))  # Hardcoding is no fun so lets ask the user for a number
+        # So you notice the int(), this is called casting. Specifically explicit casting, this will change "5" into 5.
+        # Isnt that fun
     except ValueError:  # this shows that the user has done something wrong
         print("ERROR: I dont think you know what a number is, please try again!!!")
         continue  # This tells the loop to continue and ignore everything else in the block
     break  # This code will only be called if the continue does not get hit, IE, the input is correct
 
 while counter < target:  # This is funky, were gonna keep on going while the counter is less than the target
-    # So you notice the int(), this is called casting. Specifically explicit casting, this will change "5" into 5.
-    # Isnt that fun
     total = total * counter  # Some maths stuff
     counter += 1  # Then we add 1 to the variable counter, this is using the += operator.
     print("c:", counter, "t:", total)  # And then were gonna tell the user what we have done
